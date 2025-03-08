61200a76ef83ba0c34f9760e5012a29c7977220e
willmcinnis <52384531+willmcinnis@users.noreply.github.com>
2025-03-07 23:05:44 -0800

    Add files via upload

---
+++ chatbot-frontend-main/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatInterface from './components/ChatInterface';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatInterface />
+    </div>
+  );
+}
+
+export default App;
